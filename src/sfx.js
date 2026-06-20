26619626b5755394c3621f8be80c811d14d6b901
FengkaiLiu <156460496+FengkaiLiu@users.noreply.github.com>
2026-06-19 21:56:13 -0400

    Update sfx.js

--- src/sfx.js
+++ src/sfx.js
@@ -1,6 +1,6 @@
 const _beach = new Audio('/sfx/stereogenicstudio-beach-02-404144.mp3');
 _beach.loop   = true;
-_beach.volume = 0.25;
+_beach.volume = 0.35;
 
 export function sfxStart()  { _beach.play().catch(() => {}); }
 export function sfxStop()   { _beach.pause(); }
